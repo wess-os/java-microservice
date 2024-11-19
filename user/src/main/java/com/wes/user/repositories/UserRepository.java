7b9d6e575cec25cc1b5895863dae6984c4ad6e19
Wesley <wes.lol25@gmail.com>
2024-11-19 16:12:50 -0300

    feat: added repository

---
+++ user/src/main/java/com/wes/user/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.wes.user.repositories;
+
+import com.wes.user.models.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<UserModel, UUID> {
+
+}
